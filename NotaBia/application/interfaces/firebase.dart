84c4d13165480f9a24f214bef5375f0738772b2a
miso <162466930+miso309@users.noreply.github.com>
2024-07-01 10:43:48 +0900

    add new file

--- NotaBia/application/interfaces/firebase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cheatsheet/application/types/analytics_event.dart';
-
-abstract class FirebaseService {
-  Future<void> init();
-  Future<void> sendEvent(AnalyticsEvent event);
-}
--- NotaBia/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:cheatsheet/presentation/router/app.dart';
-
-void main() {
-  const app = App();
-  const scope = ProviderScope(child: app);
-  runApp(scope);
-}
